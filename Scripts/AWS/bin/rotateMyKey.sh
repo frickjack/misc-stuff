66024d53c48e2ea6a0f22b4ec22658b5c840067e
Reuben Pasquini <reuben@frickjack.com>
2019-05-09 05:52:12 -0500

    fix(rotateMyKey): seems to work now

--- Scripts/AWS/bin/rotateMyKey.sh
+++ Scripts/AWS/bin/rotateMyKey.sh
@@ -22,13 +22,32 @@ rotateMyKey() {
   extraKeys="$(echo "$keyData" | jq -r  --arg activeKey "$activeKey" '.AccessKeyMetadata | map(select(.AccessKeyId!=$activeKey))|map(.AccessKeyId)|.[]' | head -1)"
   if [[ -n "$extraKeys" ]]; then
     echo -e "ERROR: user already has multiple keys - delete one you're not using - ex:" 1>&2
-    echo -e "    aws delete-access-key --access-key-id '$extraKeys'" 1>&2
+    echo -e "    arun aws iam delete-access-key --access-key-id '$extraKeys'" 1>&2
     return 1
   fi
-  echo $keyData | jq -r .
-
-  return $?
+  local newKeyData
+  if newKeyData="$(aws iam create-access-key)"; then
+    local credsFile
+    credsFile="$HOME/.aws/credentials"
+    if [[ -f "$credsFile" ]]; then
+      mv "$credsFile" "${credsFile}.bak"
+    fi
+    echo "Generating new creds file $credsFile" 1>&2
+    cat - > "$HOME/.aws/credentials" <<EOM
+[default]
+aws_secret_access_key = $(echo "$newKeyData" | jq -r .AccessKey.SecretAccessKey)
+aws_access_key_id = $(echo "$newKeyData" | jq -r .AccessKey.AccessKeyId)
+EOM
+    echo "Deleting old access key" 1>&2
+    aws iam delete-access-key --access-key-id "$activeKey" 1>&2
+    return $?
+  else
+    echo "ERROR: failed to create new access key" 1>&2
+    return 1
+  fi
+  return 2
 }
+
 
 if [[ -z "$GEN3_SOURCE_ONLY" ]]; then
   rotateMyKey "$@"
