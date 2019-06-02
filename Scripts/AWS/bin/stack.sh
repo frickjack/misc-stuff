d77bc1a2cc511ae901551bc5cc6cd14b233acc08
Reuben Pasquini <reuben@frickjack.com>
2019-06-02 02:31:00 -0500

    chore(arun stack): generalize
    for use with other stacks

--- Scripts/AWS/bin/stack.sh
+++ Scripts/AWS/bin/stack.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 
-if [[ ! -f "./IamSetup.json" ]]; then
-    echo "ERROR: run apply.sh within the accounSetup/ folder" 1>&2
-    exit 1
-fi
-
 set -e
+
+# globals -------------------
+
 if ! region="$(aws --profile "$profile" configure get region)"; then
     echo "ERROR: aws configure get region failed" 1>&2
     return 1
@@ -14,16 +12,12 @@ fi
 
 bucket="cloudformation-frickjack-$region"
 accountId="$(aws iam list-account-aliases | jq -r '.AccountAliases[0]')"
-stackName="little-account-iamSetup-${accountId}"
+
+# lib ------------------------
 
 help() {
-    echo "Use: bash stack.sh create|delete|events" 1>&2
+    echo "Use: bash stack.sh bucket|create|delete|events path/to/template" 1>&2
 }
-
-if [[ $# -lt 1 || $1 =~ ^-*h(elp)?$ ]]; then
-  help
-  exit 1
-fi
 
 # 
 # Apply a cloudformation update or create
@@ -34,8 +28,11 @@ apply() {
     local reqToken
     local skeleton
     local commandStr
+    local path
+    local stackName
+    local s3Path
 
-    if [[ $# -lt 1 || ! $1 =~ ^-*(update|create)$ ]]; then
+    if [[ $# -lt 3 || ! $1 =~ ^-*(update|create)$ ]]; then
       echo "ERROR: apply must specify update or create" 1>&2
       return 1
     fi
@@ -45,18 +42,23 @@ apply() {
       commandStr="create-stack"
     fi
     shift
-
+    path="$1"
+    shift
+    stackName="$1"
+    shift
+    s3Path="cf/$stackName/${path##*/}"
+   
     # uniquely identify the update-stack request
     reqToken="apply-$(date +%Y-%m-%d-%H-%M-%S)"
 
-    aws cloudformation validate-template --template-body "$(cat IamSetup.json)"
-    aws s3 sync ../accountSetup "s3://${bucket}/accountSetup/"
+    aws cloudformation validate-template --template-body "$(cat "$path")"
+    aws s3 cp "$path" "s3://${bucket}/$s3Path"
 
     skeleton="$(
         cat - <<EOM
 {
     "StackName": "$stackName",
-    "TemplateURL": "https://${bucket}.s3.amazonaws.com/accountSetup/IamSetup.json",
+    "TemplateURL": "https://${bucket}.s3.amazonaws.com/${s3Path}",
     $(if [[ "$commandStr" == "create-stack" ]]; then 
         echo '"TimeoutInMinutes": 5,'
         echo '"EnableTerminationProtection": true,'
@@ -116,20 +118,61 @@ delete() {
     aws cloudformation delete-stack --stack-name "$stackName"
 }
 
+#
+# Generate a stack name from a json/yaml template path
+#
+pathToStack() {
+    local path
+    local base
+    if [[ $# -lt 1 ]]; then
+      echo "ERROR: pathToStack takes a path" 1>&2
+      return 1
+    fi
+    path="$1"
+    shift
+    base="${path##*/}"   # get basename
+    base="${base%.*}"    # remove .json/.yaml
+    base="${base,[A-Z]}" # first character lower case
+    echo "little-account-${base}-${accountId}"
+}
+
+# main -----------------
+
+if [[ $# -lt 1 || $1 =~ ^-*h(elp)?$ ]]; then
+  help
+  exit 1
+fi
+
 command="$1"
 shift
+
+if [[ "$command" != "bucket" ]]; then
+    path="$1"
+    shift
+
+    if [[ -z "$path" || ! -f "$path" ]]; then
+        echo "ERROR: run apply.sh within the accounSetup/ folder" 1>&2
+        exit 1
+    fi
+
+    stackName="$(pathToStack "$path")"
+fi
+
 case "$command" in
+    "bucket")
+        echo "$bucket"
+        ;;
     "create")
-        create "$@"
+        create "$path" "$stackName"
         ;;
     "delete")
-        delete "$@"
+        delete "$path" "$stackName"
         ;;
     "events")
         aws cloudformation describe-stack-events --stack-name "$stackName"
         ;;
     "update")
-        update "$@"
+        update "$path" "$stackName"
         ;;
     *)
         help
