763724d27aa8da674c15ac81c5e05f37b6425494
Reuben Pasquini <reuben@frickjack.com>
2019-08-30 15:19:31 -0500

    chore(Chef): vim and unzip in base recipe

--- Chef/little-repo/cookbooks/lw_base_apt/recipes/default.rb
+++ Chef/little-repo/cookbooks/lw_base_apt/recipes/default.rb
@@ -2,7 +2,7 @@
 # apt install a bunch of random stuff ...
 #
 
-['apt-transport-https', 'curl', 'git', 'gpg', 'less', 'vim'].each do |name|
+['apt-transport-https', 'curl', 'git', 'gpg', 'less', 'nano', 'unzip', 'vim'].each do |name|
   package 'g3-base-'+name do
     package_name name
   end
