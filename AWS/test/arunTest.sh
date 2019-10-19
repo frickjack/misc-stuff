7093ea3e8389ac27804a69cb22cd28be90d87702
Reuben Pasquini <reuben@frickjack.com>
2019-10-19 17:02:41 -0500

    fix(codebuild): test suite fixes

--- AWS/test/arunTest.sh
+++ AWS/test/arunTest.sh
@@ -1,6 +1,6 @@
 
 testArunEnv() {
-    (unset AWS_SESSION_TOKEN; arun env | grep AWS_SESSION_TOKEN > /dev/null 2>&1); because $? "arun sets the AWS_SESSION_TOKEN environment"
+    (! aws configure get role_arn) || (unset AWS_SESSION_TOKEN; arun env | grep AWS_SESSION_TOKEN > /dev/null 2>&1); because $? "arun sets the AWS_SESSION_TOKEN environment"
 }
 
 
